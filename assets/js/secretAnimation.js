a939202beb1ea6f68c6579b7d3e51912e1c55655
IhebBelhajNexits <iheb.belhaj@nexits.eu>
2023-12-08 04:23:10 +0100

    working on blog pages

--- assets/js/secretAnimation.js
+++ assets/js/secretAnimation.js
@@ -8,73 +8,88 @@
   ctx.fillStyle = "red";
   ctx.fillRect(canvas.width - 50,canvas.height- 50,100 , 100);
   
-  var player1Sprite = new Image();
-  player1Sprite.src = '../animations/walkAnimation/walk1.png';
+  function load_image(path){
+    let image = new Image();
+    image.src = path;
 
-  var player2Sprite = new Image();
-  player2Sprite.src = '../animations/walkAnimation/walk1.png';
-
-  var player1 = {
-    x: 50,
-    y: canvas.height / 2,
-    width: 50,
-    height: 50,
-    speed: 5,
-    currentFrame: 0,
-    frames: 8,
-    animation: 'walk'
-  };
-
-  var player2 = {
-    x: canvas.width - 100,
-    y: canvas.height / 2,
-    width: 50,
-    height: 50,
-    speed: 5,
-    isJumping: false,
-    jumpHeight: 100,
-    deathAnimationFrames: 0,
-    currentFrame: 0,
-    frames: 8,
-    animation: 'walk'
-  };
-
-
-  function drawPlayer(player) {
-    playerSprite = player === player1 ? player1Sprite : player2Sprite;
-    var frameWidth = playerSprite.width / player.frames;
-    
-    console.log(playerSprite);
-    ctx.drawImage(
-      playerSprite,
-      frameWidth * player.currentFrame,
-      0,
-      frameWidth,
-      playerSprite.height,
-      player.x,
-      player.y,
-      player.width,
-      player.height
-    );
-  }
-
-
-  function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    drawPlayer(player1);
-    drawPlayer(player2);
+    image.onload = ()=>{
+      let aspectRatio = image.width / image.height;
+      let imageHeight = 48;
+      let imageWidth = imageHeight * aspectRatio;
+      ctx.drawImage(image, 20 , 80 , imageWidth , imageHeight);
+    }
   }
   
-  function update(){
-    draw();
-  }
-  
-  function animate(){
+  let frame;
+  let charFrame;
+  let factor=7;
 
-    update();
+   function animate(){
+    ctx.clearRect(0,0,canvas.width , canvas.height);
+    frame = (frame + 1)%(8*factor);
+    load_image(`../animations/walkAnimation/walk${charFrame}.png`)
     requestAnimationFrame(animate);
   }
   
   animate();
+
+  // var player1 = {
+  //   x: 50,
+  //   y: canvas.height / 2,
+  //   width: 50,
+  //   height: 50,
+  //   speed: 5,
+  //   currentFrame: 0,
+  //   frames: 8,
+  //   animation: 'walk',
+  //   charFrame:0,
+  // };
+
+  // var player2 = {
+  //   x: canvas.width - 100,
+  //   y: canvas.height / 2,
+  //   width: 50,
+  //   height: 50,
+  //   speed: 5,
+  //   isJumping: false,
+  //   jumpHeight: 100,
+  //   deathAnimationFrames: 0,
+  //   currentFrame: 0,
+  //   frames: 8,
+  //   animation: 'walk',
+  //   charFrame:0,
+
+  // };
+
+
+  // function drawPlayer(player) {
+  //   var player1Sprite = new Image();
+  //   player1Sprite.src = player === player1 ? '../animations/walkAnimation/walk1.png' : d ;
+    
+  //   var player2Sprite = new Image();
+  //   player2Sprite.src = '../animations/walkAnimation/walk1.png';
+
+  //   playerSprite = player === player1 ? player1Sprite : player2Sprite;
+  //   playerSprite.onLoad
+  // }
+
+
+  // function draw(){
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //   drawPlayer(player1);
+  //   drawPlayer(player2);
+  // }
+  
+  // function update(){
+  //   draw();
+  // }
+  
+  // function animate(){
+
+  //   update();
+  //   requestAnimationFrame(animate);
+  // }
+  
+  // animate();
 })()
